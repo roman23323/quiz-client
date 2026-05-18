ebfad0635442c4c35035180613a53168cffde93c
roman23323 <ignatenko_roman.s@mail.ru>
2026-05-18 13:52:50 +0300

    feat(auth): add auth screen switch logic

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -1,14 +1,34 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useAuthStore } from "../store/auth.store";
 import LoginScreen from "../screens/auth/LoginScreen";
 import HomeScreen from "../screens/home/HomeScreen";
+import { useEffect } from "react";
+import { View, ActivityIndicator } from "react-native";
 
 const Stack = createNativeStackNavigator();
 
 export default function RootNavigator() {
+  const { user, isLoading, restore } = useAuthStore();
+
+  useEffect(() => {
+    restore();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center" }}>
+        <ActivityIndicator />
+      </View>
+    );
+  }
+
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Login" component={LoginScreen} />
-      <Stack.Screen name="Home" component={HomeScreen} />
+      {user ? (
+        <Stack.Screen name="Home" component={HomeScreen} />
+      ) : (
+        <Stack.Screen name="Login" component={LoginScreen} />
+      )}
     </Stack.Navigator>
   );
 }
