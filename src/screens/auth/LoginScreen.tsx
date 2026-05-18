79269fde7b9f1e7ef3f7c25087366b8235aa35f8
roman23323 <ignatenko_roman.s@mail.ru>
2026-05-18 22:31:36 +0300

    feat(auth): add register button

--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,9 +1,20 @@
+// screens/auth/LoginScreen.tsx
 import { useState } from "react";
-import { View, Text, TextInput, Button } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  TouchableOpacity,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
 import { authApi } from "../../services/api/auth.api";
 import { useAuthStore } from "../../store/auth.store";
 
 export default function LoginScreen() {
+  const navigation = useNavigation();
+
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
 
@@ -20,13 +31,18 @@ export default function LoginScreen() {
   };
 
   return (
-    <View style={{ padding: 20 }}>
-      <Text>Login</Text>
+    <View style={{ padding: 20, gap: 12 }}>
+      <Text style={{ fontSize: 24 }}>Login</Text>
 
       <TextInput
         placeholder="имя"
         value={name}
         onChangeText={setName}
+        style={{
+          borderWidth: 1,
+          padding: 10,
+          borderRadius: 8,
+        }}
       />
 
       <TextInput
@@ -34,9 +50,28 @@ export default function LoginScreen() {
         value={password}
         onChangeText={setPassword}
         secureTextEntry
+        style={{
+          borderWidth: 1,
+          padding: 10,
+          borderRadius: 8,
+        }}
       />
 
       <Button title="Login" onPress={handleLogin} />
+
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Register" as never)}
+      >
+        <Text
+          style={{
+            marginTop: 10,
+            color: "blue",
+            textAlign: "center",
+          }}
+        >
+          Нет аккаунта? Зарегистрироваться
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
