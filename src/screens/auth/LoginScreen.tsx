330bd1945b343105e1e5daeb2c09f45c8e8fec47
roman23323 <ignatenko_roman.s@mail.ru>
2026-05-18 13:52:24 +0300

    feat(auth): add login screen

--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,10 +1,42 @@
-import { View, Text, Button } from "react-native";
+import { useState } from "react";
+import { View, Text, TextInput, Button } from "react-native";
+import { authApi } from "../../services/api/auth.api";
+import { useAuthStore } from "../../store/auth.store";
 
-export default function LoginScreen({ navigation }: any) {
+export default function LoginScreen() {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = useAuthStore((s) => s.login);
+
+  const handleLogin = async () => {
+    try {
+      const data = await authApi.login({ name, password });
+
+      await login(data.token, data.user);
+    } catch (e) {
+      console.log("Login error:", e);
+    }
+  };
+
   return (
-    <View>
-      <Text>Login Screen</Text>
-      <Button title="Go Home" onPress={() => navigation.navigate("Home")} />
+    <View style={{ padding: 20 }}>
+      <Text>Login</Text>
+
+      <TextInput
+        placeholder="имя"
+        value={name}
+        onChangeText={setName}
+      />
+
+      <TextInput
+        placeholder="пароль"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+
+      <Button title="Login" onPress={handleLogin} />
     </View>
   );
 }
