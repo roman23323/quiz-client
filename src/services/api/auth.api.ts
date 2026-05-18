8d5a262506a388be5804f29c9131c1d01ed1d302
roman23323 <ignatenko_roman.s@mail.ru>
2026-05-18 22:20:50 +0300

    feat(auth): add register route

--- src/services/api/auth.api.ts
+++ src/services/api/auth.api.ts
@@ -1,13 +1,19 @@
 import { api } from "./axios";
 
 type LoginDto = {
-  name: string;
-  password: string;
+    name: string;
+    password: string;
 };
 
 export const authApi = {
-  login: async (data: LoginDto) => {
-    const res = await api.post("/auth/login", data);
-    return res.data;
-  },
-};
+    login: async (data: LoginDto) => {
+        const res = await api.post("/auth/login", data);
+        return res.data;
+    },
+
+    register: async (dto: { name: string; password: string }) => {
+        const { data } = await api.post("/auth/register", dto);
+
+        return data;
+    }
+}
