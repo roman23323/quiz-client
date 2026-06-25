9e28bacee80349a55e9b0beff5ea0f73f04aab33
roman23323 <ignatenko_roman.s@mail.ru>
2026-06-25 20:21:42 +0300

    chore: delete server urls

--- src/services/socket/socket.ts
+++ src/services/socket/socket.ts
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const URL = "http://10.67.113.166:3000";
+const URL = "";
 
 export const socket = io(URL, {
   autoConnect: false,
